117b70458899796e42be7dea651502e7d3d15f1c
joao.yoshio <joaopedro12v@gmail.com>
2024-03-14 19:04:13 -0300

    Tratamento dos dados

--- src/controller/lol.controller.js
+++ src/controller/lol.controller.js
@@ -1,4 +1,4 @@
-import { writeFile } from 'fs/promises'
+import { writeFile, readFile } from 'fs/promises'
 import lolModel from '../model/lol.model.js'
 
 async function getPlayerData(playerName) {
@@ -43,11 +43,56 @@ async function getMatchData(matchID, puuid) {
     })
 }
 
+async function getWinRate() {
+    const data = JSON.parse(await readFile('data.json', 'utf-8'));
+
+    let count = 0;
+    data.map((match) => {
+        if (match.win == true) count++;
+    });
+    return (count * 100) / data.length;
+}
+
+async function getLaneData() {
+    const data = JSON.parse(await readFile('data.json', 'utf-8'));
+
+    const result = data.reduce((acc, match) => {
+        const position = match.individualPosition;
+        const win = match.win;
+
+        if (!acc[position]) {
+            acc[position] = { totalMatches: 0, totalWins: 0 };
+        }
+
+        acc[position].totalMatches++;
+        if (win) {
+            acc[position].totalWins++;
+        }
+        return acc;
+    }, {});
+    console.log(result);
+    return result;
+}
+
+async function getChampionData() {
+    const data = JSON.parse(await readFile('data.json', 'utf-8'));
+
+    const result = data.reduce((cont, match) => {
+        cont[match.championName] = (cont[match.championName] || 0) + 1;
+        return cont;
+    }, {});
+
+    console.log(result);
+}
+
 (async () => {
-    const puuid = await getPuuID("zYoshio", "BR1");
-    const matchID = await getMatchID(20, puuid);
-    const data = await getMatchData(matchID, puuid);
-    await writeFile("data.json", JSON.stringify(data, null, 2));
+    //const puuid = await getPuuID("zYoshio", "BR1");
+    //const matchID = await getMatchID(20, puuid);
+    //const data = await getMatchData(matchID, puuid);
+    //await writeFile("data.json", JSON.stringify(data, null, 2));
+    console.log(await getWinRate());
+    getChampionData()
+    getLaneData();
 }
 )()
 
